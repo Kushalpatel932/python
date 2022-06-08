d9c985857a4bafa6e67a432f26ed86bc8225fd69
Kushalpatel932 <78979958+Kushalpatel932@users.noreply.github.com>
2022-06-08 11:12:30 +0530

    Demonstrate how to use Python’s list comprehension syntax to produce the list [0, 2, 6, 12, 20, 30, 42, 56, 72, 90].

---
+++ tasklist.py
@@ -0,0 +1,7 @@
+
+a= []        
+for i in range(0,10):
+    a.append(i*i+i)
+print(a)
+
+# Demonstrate how to use Python’s list comprehension syntax to produce the list [0, 2, 6, 12, 20, 30, 42, 56, 72, 90].
