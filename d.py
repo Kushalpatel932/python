dcdb084e677a5efa3af684c7c9172c581d42b05d
Kushalpatel932 <78979958+Kushalpatel932@users.noreply.github.com>
2022-06-10 15:05:11 +0530

    Add files via upload

---
+++ d.py
@@ -0,0 +1,5 @@
+# Write a program to check if a number is prime or not. Prime Number is a number that is divisible only by itself and 1 (e.g. 2, 3, 5, 7, 11)
+
+for i in range(0,10):
+    if i % 2!=0:
+        print(i)
